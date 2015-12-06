20706bfbc5d6756f80d49b6f61822bbd8f60204b
Martin Pretorius <glasnoster@gmail.com>
2015-12-06 16:08:59 +0200

    Cleanup

--- lib/georama/general_parser.rb
+++ lib/georama/general_parser.rb
@@ -6,17 +6,17 @@ module Georama
       url_path = URI.parse(url).path
       split = url_path.split("/")
       raise ArgumentError, "Not a valid maps url path" if split.length < 3 || !split[2].start_with?("@")
-      result = {}
-      result[:coordinates] = get_coordinates(split[2])
-      result
+      {}.tap do |res|
+        res[:coordinates] = get_coordinates(split[2])
+      end
     end
 
     def self.get_coordinates(params)
       split_params = params.split(",")
-      result = {}
-      result[:latitude] = split_params[0].gsub("@", "").to_f
-      result[:longitude] = split_params[1].to_f
-      result
+      {}.tap do |res|
+        res[:latitude]  = split_params[0].gsub("@", "").to_f
+        res[:longitude] = split_params[1].to_f
+      end
     end
 
   end
