5c0e17a2c16cf03c70bd869fcbec3c4264619ab5
Martin Pretorius <glasnoster@gmail.com>
2015-12-06 15:07:38 +0200

    refactor

--- lib/georama/georama.rb
+++ lib/georama/georama.rb
@@ -1,17 +1,18 @@
 require 'uri'
 
-class Georama
+module Georama
+  class Parser
 
-  def self.parse(url)
-    raise ArgumentError, "Expected a valid url got nil" if url.nil?
-    parsed = URI.parse(url)
-    raise ArgumentError, "Not a valid google maps url" unless is_google_maps_url? parsed
+    def self.parse(url)
+      raise ArgumentError, "Expected a valid url got nil" if url.nil?
+      parsed = URI.parse(url)
+      raise ArgumentError, "Not a valid google maps url" unless is_google_maps_url? parsed
+    end
+
+    def self.is_google_maps_url?(parsed_url)
+      is_google = parsed_url.host == "www.google.com"
+      is_maps = parsed_url.path.start_with?("/maps/")
+      is_google && is_maps
+    end
   end
-
-  def self.is_google_maps_url?(parsed_url)
-    is_google = parsed_url.host == "www.google.com"
-    is_maps = parsed_url.path.start_with?("/maps/")
-    is_google && is_maps
-  end
-
 end
